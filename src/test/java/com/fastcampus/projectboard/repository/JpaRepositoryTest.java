ae67cc054600aaa6048acf431ad04200e39b543f
msk <msky0912@gmail.com>
2023-11-28 08:34:04 +0900

    #9 - Spring Data JPA 속성과 설정 * jpa auditing 기능을 쓰기위해 'jpaConfig' 추가 * db 접근과 개발 편의를 위한 각종 부트 프로퍼티 적용 * TODO: 패스워드를 프로퍼티에 노출시켰다. 처리필요 * 1 도메인 클래스 == 1 테이블 매칭되도록 설계했다.  생성자와 `equals()`,`hashcode()` 등이 도메인 맥락에 맞게끔 디테일하게 설정함 * jpa test 작성  기본 crud + 연관관계 매핑과 cascading 이 잘 동작하는지 보려고 작성.

--- src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
+++ src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
@@ -1,15 +1,20 @@
 package com.fastcampus.projectboard.repository;
 
 import com.fastcampus.projectboard.config.JpaConfig;
+import com.fastcampus.projectboard.domain.Article;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @DisplayName("JPA 연결 테스트")
 @Import(JpaConfig.class)
-@DataJpaTest
+@DataJpaTest // Transaction 이 걸려있어서 기본값으로 rollback 이 동작함
 class JpaRepositoryTest {
 
     private final ArticleRepository articleRepository;
@@ -27,7 +32,39 @@ class JpaRepositoryTest {
         // Given
 
         // When
+        List<Article> articles = articleRepository.findAll();
+        // Then
+        assertThat(articles).isNotNull().hasSize(0);
+    }
+
+    @DisplayName("insert test")
+    @Test
+    void givenTestData_whenInserting_thenWorksFine() {
+
+        // Given
+        long previousCount = articleRepository.count();
+
+        // When
+        articleRepository.save(Article.of("new article title", "new article content", "new hashtag"));
+        // Then
+        assertThat(articleRepository.count()).isEqualTo(previousCount + 1);
+    }
+
+    @DisplayName("update test")
+    @Test
+    void givenTestData_whenUpdating_thenWorksFine() {
+
+        // Given
+        articleRepository.save(Article.of("new article title", "new article content", "new hashtag"));
+
+        Article article = articleRepository.findById(1L).orElseThrow();
+        String updatedHashtag = "#springboot";
+        article.setHashtag(updatedHashtag);
+
+        // When
+        Article savedArticle = articleRepository.save(article);
 
         // Then
+        assertThat(savedArticle).hasFieldOrPropertyWithValue("hashtag", updatedHashtag);
     }
 }
